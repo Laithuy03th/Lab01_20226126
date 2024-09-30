a85d88224619b128914ebc104b758beaf9526ef0
Laithuy03th <167971285+Laithuy03th@users.noreply.github.com>
2024-09-30 14:34:23 +0700

    Add files via upload

---
+++ TH OOP/FirstDialog.java
@@ -0,0 +1,13 @@
+
+/**
+ * FirstDialog
+ */
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world!, How are you?");
+        System.exit(0);
+    }
+}
